1bdd9a350393358b5fec7df7a38bdbdeeb457b3f
Emmy <cyubahiroemmy12@gmail.com>
2026-01-30 07:30:55 +0200

    Demo updates

--- React-app/src/App.tsx
+++ React-app/src/App.tsx
@@ -3,14 +3,29 @@ import Alert from "./Components/Alert";
 import Button from "./Components/Button";
 import { useState } from "react";
 import { BsFillCalendarFill } from 'react-icons/bs';
+import produce from 'immer';
 
 function App() {
   const [alertVisible, setAlertVisibility] = useState(false);
+
+  const [bugs, setBugs] = useState([
+    { id: 1, title: 'Bug 1', fixed: false},
+    { id: 2, title: 'Bug 2', fixed: false},
+  ]);
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
 
   const handleSelectItem = (item: string) => {
     console.log(item);
   }
+
+  const handleClick = () => {
+    // setBugs(bugs.map(bug => bug.id === 1 ? { ...bug, fixed: true}))
+
+    setBugs(produce(draft => {
+      const bug = draft.find(bug => bug.id === 1);
+      if (bug) bug.fixed = true;
+    }))
+  };
 
   return (
     <div>
