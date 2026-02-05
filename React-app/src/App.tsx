81710cf71e1a6021a3a402db1940b2d285c4c771
Emmy <cyubahiroemmy12@gmail.com>
2026-02-05 06:03:53 +0200

    Updated

--- React-app/src/App.tsx
+++ React-app/src/App.tsx
@@ -29,7 +29,13 @@ function App() {
     toppings: ['Mushroom']
   });
 
-  const [cart, setCart] = useState();
+  const [cart, setCart] = useState({
+    discount: .1,
+    items: [
+      {id: 1, title: 'Product 1', quantity: 1},
+      {id: 2, title: 'Product 2', quantity: 1}
+    ]
+  });
   
   const [drink, setDrink] = useState({
     title: 'Americano',
