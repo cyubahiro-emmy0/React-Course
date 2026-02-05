ce2469211f4fd28b700dec6413a3ead5e334224d
Emmy <cyubahiroemmy12@gmail.com>
2026-02-05 07:05:03 +0200

    Updated

--- React-app/src/Components/ExpandableText.tsx
+++ React-app/src/Components/ExpandableText.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 
 interface Props {
   children: string;
+  maxChars? : number;
 }
 
-const ExpandableText = ({children}: Props) => {
+const ExpandableText = ({children, maxChars=100}: Props) => {
+    if(children.length <= maxChars) return <p>{children}</p>
   return (
     <div>{children}</div>
   )
-}
+};
 
 export default ExpandableText;
