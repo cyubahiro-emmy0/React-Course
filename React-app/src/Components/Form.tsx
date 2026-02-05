6b2d2911b6dd7f113f0729f764a7b295c5124e20
Emmy <cyubahiroemmy12@gmail.com>
2026-02-05 09:09:09 +0200

    Updated

--- React-app/src/Components/Form.tsx
+++ React-app/src/Components/Form.tsx
@@ -4,7 +4,7 @@ const Form = () => {
   return (
     <form>
       <div className="mb-3">
-        <label htmlFor="name" className="form-label"></label>
+        <label htmlFor="name" className="form-label">Name</label>
         <input id="name" type="text" className="form-control" />
       </div>
     </form>
