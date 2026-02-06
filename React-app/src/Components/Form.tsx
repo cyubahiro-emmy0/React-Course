6451e7ae7dfc2024a1c1e609b7d98a41227a3b9a
Emmy <cyubahiroemmy12@gmail.com>
2026-02-06 08:37:10 +0200

    Updated

--- React-app/src/Components/Form.tsx
+++ React-app/src/Components/Form.tsx
@@ -17,13 +17,13 @@ const Form = () => {
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input ref={nameRef} id="name" type="text" className="form-control" />
+        <input  id="name" type="text" className="form-control" />
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
-        <input  ref={ageRef} id="age" type="number" className="form-control" />
+        <input  id="age" type="number" className="form-control" />
       </div>
       <button className="btn btn-primary" type="submit">
         Submit
