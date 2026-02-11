bf4785220fa4583e82ca2855fdafa0d52b769714
Emmy <cyubahiroemmy12@gmail.com>
2026-02-11 14:58:06 +0200

    Updated the expense list

---
+++ React-app/src/expense-tracker/components/ExpenseFilter.tsx
@@ -0,0 +1,17 @@
+
+interface Props{
+    onSelectCategory: (category: String) => void;
+}
+
+const ExpenseFilter = ({onSelectCategory}: Props) => {
+  return (
+    <select  className="form-select" onChange={(event) => onSelectCategory(event.target.value) }>
+        <option value="">All Categories</option>
+        <option value="groceries">Groceries</option>
+        <option value="utilities">Utilities</option>
+        <option value="entertainment">Entertainment</option>
+    </select>
+  )
+}
+
+export default ExpenseFilter
