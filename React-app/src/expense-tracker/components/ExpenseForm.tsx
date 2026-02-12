42d257b6197df11e873f14692f49fc7031c54b00
Emmy <cyubahiroemmy12@gmail.com>
2026-02-12 19:34:43 +0200

    Updated

--- React-app/src/expense-tracker/components/ExpenseForm.tsx
+++ React-app/src/expense-tracker/components/ExpenseForm.tsx
@@ -8,10 +8,15 @@ const ExpenseForm = () => {
         <input id="description" type="text" className="form-control" />
       </div>
       <div className="mb-3">
-        <label htmlFor="amount" className="form-label">Amount</label>
+        <label htmlFor="amount" className="form-label">
+          Amount
+        </label>
         <input id="amount" type="number" className="form-control" />
       </div>
-      <div className="mb-3"><label htmlFor="" className="form-label"></label><select name="" id="" className="form-select"></select></div>
+      <div className="mb-3">
+        <label htmlFor="" className="form-label"></label>
+        <select name="" id="" className="form-select"></select>
+      </div>
     </form>
   );
 };
